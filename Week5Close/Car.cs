bb1235666443c973f6a83a2fd2edc24677f36a85
Kaan Kaya <kaankaya.0707@gmail.com>
2024-08-14 03:47:27 +0300

    Add project files.

---
+++ Week5Close/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week5Close
+{
+    public class Car
+    {
+        //Property Tanımları 
+        public DateTime ProductionDate { get; set; }
+        public string SerialNumber { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int NumberDoors { get; set; }
+        //Constructor Methodu tanımı -Kullanıcı oluşturucağı zaman veri eşitlenme işlemi yapımı
+        public Car(string serialNumber, string brand, string model, string color, int numberDoors)
+        {
+            //üretim tarihini o anın tarihini atama
+            ProductionDate = DateTime.Now;
+            SerialNumber = serialNumber;
+            Brand = brand;
+            Model = model;
+            Color = color;
+            NumberDoors = numberDoors;
+        }
+        //Araç oluşturma methodu kullanıcıdan bilgileri alma
+        //kapı sayısı kontrolü
+        public static Car CreateCar()
+        {
+            //Tolower ve trim ile kullanıcıdan girilen verilerin formatlanması
+            Console.Write("Seri Numarası: ");
+            string seriNumarasi = Console.ReadLine().Trim().ToLower();
+
+            Console.Write("Marka: ");
+            string marka = Console.ReadLine().Trim().ToLower();
+
+            Console.Write("Model: ");
+            string model = Console.ReadLine().Trim().ToLower();
+
+            Console.Write("Renk: ");
+            string renk = Console.ReadLine().Trim().ToLower();
+
+            int numberOfDoors;
+            while (true)
+            {
+                Console.Write("Kapı Sayısı: ");
+                string numberDoor = Console.ReadLine();
+
+                if (int.TryParse(numberDoor, out numberOfDoors))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Kapı Sayısı sadece sayısal bir değer olabilir. Lütfen tekrar deneyin.");
+                }
+            }
+
+            return new Car(seriNumarasi, marka, model, renk, numberOfDoors);
+        }
+    }
+}
---
+++ Week5Close/Program.cs
@@ -0,0 +1,42 @@
+﻿using Week5Close;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //generic list oluşturumu
+        List<Car> cars = new List<Car>();
+        //başka bir araç daha üretmek isterse while döngüsü ile başlangıcı
+        while (true)
+            //goto için etiket tanımı
+        {   CreateCar:
+            Console.WriteLine("Araç Üretmek İstiyormusunuz.Cevap için :\n[e] Evet\n[q] Hayır");
+            //tolower ve trim ile giriş formatlanması
+            string input = Console.ReadLine().ToLower().Trim();
+
+            if(input == "e")
+            {
+                //newCar adında constructor ile nesnenin oluşumu ve generic listenin içine aktarılması
+                Car newCar = Car.CreateCar();
+                cars.Add(newCar);
+            }else if(input == "q")
+            {
+                Console.WriteLine("Hoşca Kalın");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Cevap olarak sadece e veya q giriniz");
+                goto CreateCar;
+            }
+        }//End-while
+        //üretilen araçların listelenmesi foreach ile liste içinde dönülmesi
+        Console.WriteLine("\nÜretilmiş Araçlar:");
+        Console.WriteLine("***********************************");
+        foreach (var item in cars)
+        {
+            Console.WriteLine($"Üretim Tarihi : {item.ProductionDate}\nSeri Numarası: {item.SerialNumber}\nMarka: {item.Brand}\nModeli : {item.Model}\nRengi : {item.Color}\nKapı Sayısı : {item.NumberDoors}");
+            Console.WriteLine("*****************************************");
+        }
+    }
+}
